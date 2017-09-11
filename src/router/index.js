677761fe2258910c1d6fd379681b5144d73456f7
wangmingwei <wangmw@bbtree.com>
2017-09-11 20:19:26 +0800

    frame ok

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import VueRouter from 'vue-router'
+import {routerMode} from '../config/env'
+import App from '../App'
+import home from '../components/page/home/home'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+export default new VueRouter({
+  // history: true,
+  mode: routerMode,
+  strict: process.env.NODE_ENV !== 'production',
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      if (from.meta.keepAlive) {
+        from.meta.savedPosition = document.body.scrollTop;
+      }
+      return { x: 0, y: to.meta.savedPosition ||0}
     }
-  ]
+  },
+  routes: [{
+    path: '/',
+    component: App, //顶层路由，对应index.html
+    children: [ //二级路由。对应App.vue
+      //地址为空时跳转home页面
+      {
+        path: '',
+        redirect: '/home'
+      },
+      //首页城市列表页
+      {
+        path: '/home',
+        component: home
+      },
+
+    ]
+  }]
 })
+
